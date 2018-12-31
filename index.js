42b1a339f7577e82ce17bed7eea96279b85dae67
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-31 23:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function createPost() {
-  let pageTemplate = _.template(document.getElementById("page-template"))
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML)
 }
 
 function postComment() {
