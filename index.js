c33812a96e3684dd8452740051626e9dd3e86157
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-01 00:24:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function createPost() {
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  
+  let postTitle = 
 }
 
 function postComment() {
