dca2cd97e426cd6b2e5eb9a16be354d012eb6007
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-01 00:41:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function createPost() {
-  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
-  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  
-  let postTitle = document.getElementById("postTitle").value;
-  let postAuthor = document.getElementById("postAuthor").value;
-  let post = document.getElementById("post").value;
-  
-  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
-  
-  let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
-  let commentsSection = commentsTemplate();
-  let postElement = document.getElementById("post");
+   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
  
-  postElement.innerHTML = blogSection;
+    var postTitle = document.getElementById("postTitle").value;
+   var postAuthor = document.getElementById("postAuthor").value;
+   var post = document.getElementById("postBody").value;
+ 
+    document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+ 
+    var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+   var commentsSection = commentsTemplate();
+   var postElement = document.getElementById("post");
+ 
+    postElement.innerHTML = blogSection;
    postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
